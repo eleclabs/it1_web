5d28c21a918e1cd22cf20f61f3403951a3bcceca
eleclabs <eleclabs@gmail.com>
2026-08-27 09:31:45 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import BlogCard from "@/components/BlogCard";
 
 export default function Home() {
   return (
@@ -7,8 +6,6 @@ export default function Home() {
     
       <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
       <p> โดย สมเกียรติ ใจดี </p>
-
-       <BlogCard/>
  
     </div>
   );
